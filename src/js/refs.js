d5f5e1ecca82a01cd78b1dfdb29201ba2cc3bcde
LexBondarchyk <108199972+LexBondarchyk@users.noreply.github.com>
2022-12-08 21:30:57 +0200

    sf
    1
    dfg

--- src/js/refs.js
+++
@@ -1,9 +0,0 @@
-const searchForm = document.querySelector('#search-form');
-const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('button[data-action="load-more"]');
-
-export default {
-  searchForm,
-  gallery,
-  loadMoreBtn,
-};
